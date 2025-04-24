d017f4f2075d27007bc10902c8ee87c2ecc98170
Alvaro Burgos <alvarobda11@gmail.com>
2025-04-24 14:28:02 +0200

    cliente

---
+++ src/Cliente.java
@@ -0,0 +1,70 @@
+public class Cliente {
+    int id;
+    String dni;
+    String nombre;
+    String telefono;
+    String email;
+
+
+    public Cliente(){}
+
+    public Cliente(String dni, String nombre, String telefono, String email) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
