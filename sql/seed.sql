b7989f957d770203a571b2565e7ece9c1f2149b8
Fabricio Oliva <fabricio.s399523@cesurformacion.com>
2025-05-06 13:02:23 +0200

    Change: Corrigiendo nomenclatura
    Los nombres de las tablas, y las columnas no estaban correctamente
    estandarizadas, tenían diferentes case, eran singular y otras plural,
    mayúsculas y minúsculas, etc. Las puse todas igual aunque aún estoy
    indeciso sobre que usuario está en inglés cómo user a diferencia de las
    demás tablas en español.

--- sql/seed.sql
+++ sql/seed.sql
@@ -9,7 +9,7 @@ CREATE TABLE Productos (
     marca VARCHAR(100) NOT NULL,
     categoria VARCHAR(100) NOT NULL
 );
-CREATE TABLE Cliente (
+CREATE TABLE Clientes (
     idCliente INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     apellido VARCHAR(255) NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE Cliente (
     direccion VARCHAR(255),
     telefono VARCHAR(20)
 );
-CREATE TABLE empleados (
-    id_empleado INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Empleados (
+    idEmpleado INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     dni VARCHAR(20) UNIQUE,
@@ -27,17 +27,17 @@ CREATE TABLE empleados (
     telefono VARCHAR(20),
     email VARCHAR(100)
 ); 
-CREATE TABLE Descuento (
+CREATE TABLE Descuentos (
     idDescuento INT AUTO_INCREMENT PRIMARY KEY,
     descripcion TEXT,
-    porcentaje_fija BOOLEAN NOT NULL,
+    porcentajeFija BOOLEAN NOT NULL,
     porcentaje DECIMAL(5, 2),
-    cantidad_fija DECIMAL(10, 2)
+    cantidadFija DECIMAL(10, 2)
 );
-CREATE TABLE Inventario (
+CREATE TABLE Inventarios (
     idInventario INT AUTO_INCREMENT PRIMARY KEY
 );
-CREATE TABLE Pedido (
+CREATE TABLE Pedidos (
     idPedido INT AUTO_INCREMENT PRIMARY KEY,
     idCliente INT,
     fechaPedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -45,28 +45,28 @@ CREATE TABLE Pedido (
     estado BOOLEAN NOT NULL,
     total DECIMAL(10, 2) NOT NULL,
     direccion VARCHAR(255),
-    FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente)
+    FOREIGN KEY (idCliente) REFERENCES Clientes(idCliente)
 );
 CREATE TABLE User (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    idUser INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     admin BOOLEAN NOT NULL
 );
-CREATE TABLE  Cajas (
-    id_caja INT AUTO_INCREMENT PRIMARY KEY,
-    fecha_cierre DATE UNIQUE,
-    saldo_inicial DECIMAL(10, 2),
+CREATE TABLE Cajas (
+    idCaja INT AUTO_INCREMENT PRIMARY KEY,
+    fechaCierre DATE UNIQUE,
+    saldoInicial DECIMAL(10, 2),
     gastos DECIMAL(10, 2),
-    saldo_total DECIMAL(10, 2)
+    saldoTotal DECIMAL(10, 2)
 );
 CREATE TABLE Proveedores (
-    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    idProveedor INT AUTO_INCREMENT PRIMARY KEY,
     nombre_empresa VARCHAR(255) NOT NULL,
     cif VARCHAR(20) UNIQUE,
     direccion VARCHAR(255),
-    telefono_contacto VARCHAR(20),
-    email_contacto VARCHAR(100)
+    telefonoContacto VARCHAR(20),
+    emailContacto VARCHAR(100)
 );
 CREATE TABLE DetallePedido (
     idDetallePedido INT AUTO_INCREMENT PRIMARY KEY,
@@ -86,6 +86,14 @@ CREATE TABLE InventarioProducto (
     ubicacion VARCHAR(100) NULL,
     cantidad INT UNSIGNED NOT NULL,
     fechaUltimaActualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (idInventario) REFERENCES Inventario(idInventario),
+    FOREIGN KEY (idInventario) REFERENCES Inventarios(idInventario),
     FOREIGN KEY (idProducto) REFERENCES Productos(idProducto)
 );
+CREATE TABLE Carrito (
+    idCarrito INT AUTO_INCREMENT PRIMARY KEY,
+    idCliente INT,
+    idProducto INT,
+    cantidad INT,
+    FOREIGN KEY (idCliente) REFERENCES Clientes(idCliente),
+    FOREIGN KEY (idProducto) REFERENCES Productos(idProducto)
+)
